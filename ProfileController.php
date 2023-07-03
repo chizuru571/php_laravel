0814344cde97482c6069cd91b384eb0dd04ca133
chizuru <rcbu7410@gmail.com>
2023-07-03 04:58:04 +0000

    プロフィールデータを保存する

--- ProfileController.php
+++ ProfileController.php
@@ -5,15 +5,31 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profiles;
+
 class ProfileController extends Controller
 {//
         public function add()
     {
         return view('admin.profile.create');
     }
-
-    public function create()
+    
+    public function create(Request $request)
     {
+        // 以下を追記
+        // Validationを行う
+        $this->validate($request, Profiles::$rules);
+
+        $profile = new Profiles;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
 
